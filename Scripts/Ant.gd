d98f019da3ae9575d9f7dd40f917afb2271f5f4e
André Decher <andre.decher@gmail.com>
2018-12-02 00:01:27 +0100

    some progress

--- Scripts/Ant.gd
+++ Scripts/Ant.gd
@@ -1,15 +1,41 @@
 extends KinematicBody2D
 
-export var speed = 50
+export var speed = 100
 
-const GRAVITY = 10
+const GRAVITY = 200
 const UP = Vector2(0, -1)
 
-var velocity = Vector2(1, 0)
+var velocity = Vector2()
+var move_direction = Vector2(1,0)
 
+
+func _ready():
+	$Timer.start()
+	
+
+func _process(delta):
+	#var time_left = $Timer.time_left
+	#if time_left <= 2:
+	#	$Label.add_color_override("font_color", Color(1,0,0))
+	$Label.text = str($Timer.time_left).pad_decimals(0)
 
 func _physics_process(delta):
 	if is_on_floor():
 		velocity.y = 0
+	if is_on_wall():
+		toogle_move_direction()
 	velocity.y += delta * GRAVITY
-	move_and_slide(velocity * speed, UP)
+	velocity.x = speed * move_direction.x
+	move_and_slide(velocity, UP)
+	
+func toogle_move_direction():
+	move_direction.x *= -1
+	$Sprite.flip_h = !$Sprite.flip_h 
+
+func _on_Timer_timeout():
+	# EXPLOSION
+	var explosion = load("res://Environment/Explosion.tscn").instance()
+	get_parent().add_child(explosion)
+	explosion.position = self.position
+	queue_free()
+	#pass
--- Scripts/AntSpawner.gd
+++ Scripts/AntSpawner.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 func _on_Timer_timeout():
-	var ant = load("res://Characters/Ant.tscn").instance()	
+	var ant = load("res://Characters/Ant.tscn").instance()
+	ant.position = $Position2D.position
 	add_child(ant)
